9e1a6c0281da97b31218eb819ac3a3513e8862da
thiago3100 <thfelipe31@gmail.com>
2022-07-07 15:51:12 -0300

    subindo desafio

---
+++ Account.cs
@@ -0,0 +1,45 @@
+using System.Globalization;
+namespace Desafio
+{
+    public class Account
+    {
+        private string _name;
+        public int NumAccount{get; private set;}
+        public double DepositedAmount {get; private set;}
+        public Account(){}
+        public Account(int numAccount, string name, double depositeAmount)
+        {
+            _name = name;
+            NumAccount = numAccount;
+            DepositedAmount = depositeAmount;
+        }
+        public Account(string name, int numAccount) : this(){}
+        public string Name{
+            get {return _name;}
+            set {
+                if(value != null && value.Length > 3){
+                    _name = value;
+                }
+            }
+        }
+        public double Deposite(double depositeAmount)
+        {
+            return DepositedAmount += depositeAmount;
+        }
+        public double Balance(double depositeAmount)
+        {
+            return DepositedAmount -= depositeAmount + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+            + NumAccount
+            +" , "
+            +"Titular: "
+            +_name
+            +" , "
+            + "Saldo: "+ DepositedAmount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+namespace Desafio
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Account account = new Account();
+            Console.Write("Entre com o número da conta: ");
+            int numAccount = int.Parse(Console.ReadLine());
+            
+            Console.Write("Entre com o nome do titular: ");
+            account.Name = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char choice = char.Parse(Console.ReadLine());
+
+            double Amount= 0.0;
+
+            while(choice != 's' && choice != 'n')
+            {
+                Console.WriteLine("Digito inválido, por favor digitar novamente: ");
+                Console.WriteLine();
+                Console.Write("Haverá depósito inicial (s/n)? ");
+                Console.WriteLine();
+                choice = char.Parse(Console.ReadLine());
+
+                if(choice == 's')
+                {
+                    Console.WriteLine();
+                    Console.Write("Entre o valor de depósito inicial: ");
+                    Amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Account readAccount = new Account(numAccount, account.Name, Amount );
+
+            Console.WriteLine();
+
+            Console.WriteLine("Entre com os dados da Conta: ");
+            Console.WriteLine(readAccount);
+
+            Console.WriteLine();
+            
+            Console.Write("Entre um valor para depósito: ");
+            double valueDep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            readAccount.Deposite(valueDep);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(readAccount);
+            
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            readAccount.Balance(value);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(readAccount);
+        }
+    }
+}
